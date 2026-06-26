ffe880ef06a4dd806d19621a2b0cb972d48e2c54
CreativeRepos <56748187+CreativeRepos@users.noreply.github.com>
2026-06-26 22:01:43 +0530

    Corrected options

--- aws_vpc_create.sh
+++ aws_vpc_create.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 #########################################
-# Description: Create VPC in AWS
-# - Create VPC
-# - Create a public subnet
-# 
-# - Verify if user has AWS installed, User might be using windows, linux or mac.
-# - verify if AWS CLI is configured
-# - Usage: ./aws_vpc_create.sh
-# - options: deleteVPC - delete the VPC created by this script,
-#            deleteSubnet - delete the subnet created by this script
-#            --help - display this help message
-# 
-#
+# Description: Create or manage VPC in AWS
+# Author: Mani
+# Options:
+#   create        Create VPC and subnet
+#   deleteVPC     Delete the VPC created by this script
+#   deleteSubnet  Delete the subnet created by this script
+#   --help        Display this help message
 #########################################
 
 # Variables
@@ -20,33 +15,31 @@ VPC_NAME="vpc-demo"
 SUBNET_NAME="subnet-demo"
 REGION="us-east-1"
 VPC_CIDR="10.0.0.0/16"
-SUBNET_NAME="10.0.3.0/24"
+SUBNET_CIDR="10.0.3.0/24"
 SUBNET_AZ="us-east-1a"
 
 # Check if AWS CLI is installed
-if ! command -v aws &> /dev/null
-then
+if ! command -v aws &> /dev/null; then
     echo "AWS CLI could not be found. Please install AWS CLI before running this script."
     exit 1
 fi
 
 # Check if AWS CLI is configured
-if ! aws sts get-caller-identity &> /dev/null
-then
+if ! aws sts get-caller-identity &> /dev/null; then
     echo "AWS CLI is not configured. Please configure AWS CLI with your credentials before running this script."
     exit 1
 fi
 
-# Create VPC
-echo "Creating VPC..."
-VPC_ID=$(aws ec2 create-vpc --cidr-block $VPC_CIDR --query 'Vpc.VpcId' --output text --region $REGION)
-echo "VPC created with ID: $VPC_ID" 
+# Functions
+createVPC() {
+    echo "Creating VPC..."
+    VPC_ID=$(aws ec2 create-vpc --cidr-block $VPC_CIDR --query 'Vpc.VpcId' --output text --region $REGION)
+    echo "VPC created with ID: $VPC_ID"
 
-# Create a public subnet
-echo "Creating public subnet..."  
-SUBNET_ID=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block $SUBNET_CIDR --availability-zone $SUBNET_AZ --query '
-Subnet.SubnetId' --output text --region $REGION)
-echo "Public subnet created with ID: $SUBNET_ID"
+    echo "Creating public subnet..."
+    SUBNET_ID=$(aws ec2 create-subnet --vpc-id $VPC_ID --cidr-block $SUBNET_CIDR --availability-zone $SUBNET_AZ --query 'Subnet.SubnetId' --output text --region $REGION)
+    echo "Public subnet created with ID: $SUBNET_ID"
+}
 
 deleteVPC() {
     echo "Deleting VPC with ID: $VPC_ID"
@@ -63,9 +56,27 @@ deleteSubnet() {
 help() {
     echo "Usage: ./aws_vpc_create.sh [options]"
     echo "Options:"
+    echo "  create        Create VPC and subnet"
     echo "  deleteVPC     Delete the VPC created by this script"
     echo "  deleteSubnet  Delete the subnet created by this script"
     echo "  --help        Display this help message"
 }
 
-# End of script
+# Main logic
+case "$1" in
+    create)
+        createVPC
+        ;;
+    deleteVPC)
+        deleteVPC
+        ;;
+    deleteSubnet)
+        deleteSubnet
+        ;;
+    --help)
+        help
+        ;;
+    *)
+        echo "Invalid option. Use --help for usage."
+        ;;
+esac
